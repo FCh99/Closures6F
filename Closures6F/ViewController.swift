f0d1a79b5b8460d289bdb7012c736adb332053ee
Fausto Checa <fausto.checa@gmail.com>
2017-09-06 23:07:22 +0200

    Initial Commit

---
+++ Closures6F/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Closures6F
+//
+//  Created by Fausto Checa on 6/9/17.
+//  Copyright © 2017 Fausto Checa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
